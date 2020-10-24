e43274af1dd99f679e64abf3cd64a1fc135e09b3
Karla Adlemy de la Cruz Pérez <kaarlacp@hotmail.com>
2020-10-23 20:50:27 -0500

    File creation
    Se creó archivo perimetro_triangulo.c.

---
+++ exercises/perimetro_triangulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float a, b, c, P; //Declarando variables de tipo flotante
+	printf("Ingresa la longitud del lado a: ");
+	scanf("%f", &a);
+	printf("Ingresa la longitud del lado b: ");
+	scanf("%f", &b);
+	printf("Ingresa la longitud del lado c: ");
+	scanf("%f", &c);
+	printf("Perimetro\n"); //Imprimiendo título
+		P=a+b+c; //Suma de los lados del triángulo
+		printf("\tEl perimetro del triangulo es = %0.2f.\n",P);
+return 0;	
+}
